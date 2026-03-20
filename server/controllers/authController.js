2bb69812d8d49e8d4de82131ead2320feec4c8cd
James Isaac <deelordpopdy2@gmail.com>
2026-03-20 10:29:16 +0100

    Update authController.js
    Safe

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -35,13 +35,13 @@ const sendMail = async (req, res) => {
     const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: "marketcoin823@gmail.com",
+        pass: "znky vegp xxuo lign",
       },
     });
 
     const mailOptions = {
-      from: process.env.EMAIL_USER,
+      from: "marketcoin823@gmail.com",
       to: email,
       subject,
       text: message,
